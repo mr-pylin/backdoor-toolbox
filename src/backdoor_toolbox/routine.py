4b6d380a9fdd5f25bb34008f26c47349dfed31ec
Amirhossein Heydari <amirhosseinheydari78@gmail.com>
2025-01-18 21:15:38 +0330

    remove unnecessary imports

--- src/backdoor_toolbox/routine.py
+++ src/backdoor_toolbox/routine.py
@@ -1,25 +1,18 @@
-import torch
-from torch import optim
-from torchvision.transforms import v2
-
-from triggers.target_transform import TargetTriggerTypes
-from triggers.transform import TriggerTypes
-
 # uncomment the method you want to execute
 # path : from "root"."file" import "class"
 
 # backdoor_toolbox/routines/neutral/
-# routine = {
-#     "root": "backdoor_toolbox.routines.neutral",
-#     "file": "neutral",
-#     "class": "NeutralRoutine",
-#     "verbose": True,
-# }
-
-# backdoor_toolbox/routines/attacks/quantization_attack/
 routine = {
-    "root": "backdoor_toolbox.routines.attacks.quantization_attack",
-    "file": "quantization_attack",
-    "class": "QuantizationAttackRoutine",
+    "root": "backdoor_toolbox.routines.neutral",
+    "file": "neutral",
+    "class": "NeutralRoutine",
     "verbose": True,
 }
+
+# backdoor_toolbox/routines/attacks/quantization_attack/
+# routine = {
+#     "root": "backdoor_toolbox.routines.attacks.quantization_attack",
+#     "file": "quantization_attack",
+#     "class": "QuantizationAttackRoutine",
+#     "verbose": True,
+# }
