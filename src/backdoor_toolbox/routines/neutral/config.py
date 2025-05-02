c08b52ea175aeb38c495fcde4dec186a3d98c4e7
Amirhossein Heydari <amirhosseinheydari78@gmail.com>
2025-05-02 17:13:03 +0330

    Refactor neutral model trainer

--- src/backdoor_toolbox/routines/neutral/config.py
+++ src/backdoor_toolbox/routines/neutral/config.py
@@ -4,36 +4,37 @@ from torchvision.transforms import v2
 
 
 config = {
-    "dataset": {
-        "root": "./data",
-        "train": True,
-        "normalize": True,  # mean and std is computed dynamically based on chosen dataset
-        "transform": v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
-        "target_transform": None,
-        "download": False,
-        "num_classes": 10,
-        "image_shape": (1, 28, 28),  # consider `image_shape` after passing through `v2.ToImage()`
-    },
+    # `from root.file import class`
     "modules": {
+        # check ./datasets/ for available datasets
         "dataset": {
-            # from "root"."file" import "class"
             "root": "datasets",
             "file": "mnist",
-            "class": "CleanMNIST",
+            "class": "MNIST",
         },
+        # check ./models/ for available models
         "model": {
-            # from "root"."file" import "class"
             "root": "models.cnn",
             "file": "resnet_wrapper",
             "class": "CustomResNet",
             "params": {
-                "weights": None,  # e.g. "ResNet18_Weights.IMAGENET1K_V1" or path to .pth file
-                "kwargs": {"model_name": "resnet18"},
+                "arch": "resnet18",
+                "weights": None,  # options: {None, True, ".pth file"}
             },
         },
     },
+    "dataset": {
+        "root": "./data",
+        "transform": v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+        "target_transform": None,
+        "download": False,
+        "image_shape": (1, 28, 28),  # consider `image_shape` after passing through `v2.ToImage()`
+        "normalize": True,  # whether to use v2.Normalize() or not
+        "num_classes": 10,
+    },
     "train_val": {
-        "train_val_ratio": (0.9, 0.1),  # 0.9 for train set and 0.1 for validation set
+        "train_test_ratio": (0.8, 0.2),  # (train, test) split
+        "train_val_ratio": (0.8, 0.2),  # (train, validation) split
         "epochs": 15,
         "train_batch_size": 64,
         "val_batch_size": 128,
@@ -41,24 +42,58 @@ config = {
         "optimizer_params": {"lr": 0.01},
         "scheduler_params": {"mode": "min", "factor": 0.5, "patience": 2, "threshold": 1e-3},
     },
-    "test": {"test_batch_size": 128},
+    "test": {
+        "test_batch_size": 128,
+    },
     "log": {
-        "root": "./logs/neutral",
+        "root": "./logs/neutral/neutral",
         "include_date": True,
-        "config": {"path": "src/backdoor_toolbox/routines/neutral", "filename": "config"},
-        "hyperparameters": {"path": "train_val", "filename": "hyperparameters"},
-        "metrics": {"train_path": "train_val", "test_path": "test", "filename": "report"},
-        "weights": {"path": "train_val/weights"},
+        "config": {
+            "path": "src/backdoor_toolbox/routines/neutral",
+            "filename": "config",
+        },
+        "hyperparameters": {
+            "path": "train_val",
+            "filename": "hyperparameters",
+        },
+        "metrics": {
+            "train_path": "train_val",
+            "test_path": "test",
+            "filename": "report",
+        },
+        "weights": {
+            "path": "train_val/weights",
+            "only_state_dict": True,
+        },
         "plot": {
             "path": "train_val/plots",
             "save_format": "svg",
             "metrics": [
-                {"filename": "loss", "ylabel": "Loss", "title": "Loss over time", "show": False},
-                {"filename": "accuracy", "ylabel": "Accurace", "title": "Accuracy over time", "show": False},
+                {
+                    "filename": "loss",
+                    "ylabel": "Loss",
+                    "title": "Loss over time",
+                    "show": False,
+                },
+                {
+                    "filename": "accuracy",
+                    "ylabel": "Accurace",
+                    "title": "Accuracy over time",
+                    "show": False,
+                },
             ],
         },
-        "confusion_matrix": {"path": "test", "filename": "confusion_matrix"},
-        "demo": {"train_path": "train_val/demo", "test_path": "test/demo", "nrows": 7, "ncols": 20, "show": False},
+        "confusion_matrix": {
+            "path": "test",
+            "filename": "confusion_matrix",
+        },
+        "demo": {
+            "train_path": "train_val/demo",
+            "test_path": "test/demo",
+            "nrows": 8,
+            "ncols": 24,
+            "show": False,
+        },
     },
     "misc": {
         "seed": 42,
--- src/backdoor_toolbox/routines/neutral/neutral.py
+++ src/backdoor_toolbox/routines/neutral/neutral.py
@@ -11,10 +11,8 @@ from backdoor_toolbox.routines.base import BaseRoutine
 from backdoor_toolbox.routines.neutral.config import config
 from backdoor_toolbox.utils.logger import Logger
 
-# global configuration file
-config: dict[str,] = config
 
-# global logger
+# instantiate a logger to save parameters, plots, weights, ...
 logger = Logger(
     root=config["log"]["root"],
     include_date=config["log"]["include_date"],
@@ -23,75 +21,63 @@ logger = Logger(
 
 
 class NeutralRoutine(BaseRoutine):
+
     def __init__(self):
-
-        # extract common values from config file
-        self.num_classes = config["dataset"]["num_classes"]
-        self.seed = config["misc"]["seed"]
-        self.device = config["misc"]["device"]
-        self.verbose = config["misc"]["verbose"]
-
         # set manual seed
-        torch.manual_seed(self.seed)
+        torch.manual_seed(config["misc"]["seed"])
 
         # save current config in the log
-        logger.save_configs(
-            config["log"]["config"]["path"],
-            config["log"]["config"]["filename"],
-        )
+        logger.save_configs(config["log"]["config"]["path"], config["log"]["config"]["filename"])
 
-        # future attributes
-        self.train_set: Dataset
-        self.val_set: Dataset
-        self.test_set: Dataset
-        self.train_loader: DataLoader
-        self.val_loader: DataLoader
-        self.test_loader: DataLoader
-        self.model: nn.Module
+        # initialize data loaders
+        # test_loader = DataLoader(test_set, batch_size=config["test"]["test_batch_size"], shuffle=False)
 
     def apply(self) -> None:
-        # prepare datasets and dataloaders
-        datasets, dataloaders = self._prepare_data(
-            module_path=f"{config["modules"]["dataset"]["root"]}.{config["modules"]["dataset"]["file"]}",
-        )
-        self.train_set, self.val_set, self.test_set = datasets
-        self.train_loader, self.val_loader, self.test_loader = dataloaders
+        # prepare datasets
+        train_set, val_set, test_set = self._prepare_data()
 
-        # prepare model
-        self.model = self._prepare_model(
-            module_path=f"{config["modules"]["model"]["root"]}.{config["modules"]["model"]["file"]}",
-            module_cls=config["modules"]["model"]["class"],
-            weights=config["modules"]["model"]["params"]["weights"],
-        )
+        # prepare model and initialize weights
+        model = self._prepare_model()
 
         # train and validate
-        self._train_and_validate()
+        self._train_and_validate(train_set, val_set, model)
 
         # test
-        self._test()
+        self._test(test_set, model)
 
-    def _prepare_data(
-        self,
-        module_path: str,
-    ) -> tuple[tuple[Dataset, ...], tuple[DataLoader, ...]]:
+    def _prepare_data(self) -> tuple[Dataset, Dataset, Dataset]:
 
         # import dataset class
-        dataset_cls = getattr(self._import_package(module_path), config["modules"]["dataset"]["class"])
+        dataset_cls = getattr(
+            self._import_package(f"{config["modules"]["dataset"]["root"]}.{config["modules"]["dataset"]["file"]}"),
+            config["modules"]["dataset"]["class"],
+        )
 
         # initialize train and validation set
         train_set = dataset_cls(
             root=config["dataset"]["root"],
-            train=config["dataset"]["train"],
+            train=True,
             transform=config["dataset"]["transform"],
             target_transform=config["dataset"]["target_transform"],
             download=config["dataset"]["download"],
-            seed=self.seed,
+        )
+
+        # initialize test set
+        test_set = dataset_cls(
+            root=config["dataset"]["root"],
+            train=False,
+            transform=config["dataset"]["transform"],
+            target_transform=config["dataset"]["target_transform"],
+            download=config["dataset"]["download"],
         )
 
         # split train set into train and validation set
-        train_set, val_set = random_split(train_set, config["train_val"]["train_val_ratio"])
+        train_set, val_set = random_split(
+            train_set,
+            config["train_val"]["train_val_ratio"],
+        )
 
-        # calculate mean and std per channel add `v2.Normalize` to transforms
+        # normalize (standardize) samples if needed
         if config["dataset"]["normalize"]:
             self.mean_per_channel, self.std_per_channel = self._calculate_train_set_mean_and_std(train_set)
             config["dataset"]["transform"].transforms.append(v2.Normalize(self.mean_per_channel, self.std_per_channel))
@@ -99,59 +85,51 @@ class NeutralRoutine(BaseRoutine):
             self.mean_per_channel = None
             self.std_per_channel = None
 
-        # initialize test set
-        test_set = dataset_cls(
-            root=config["dataset"]["root"],
-            train=not config["dataset"]["train"],
-            transform=config["dataset"]["transform"],
-            target_transform=config["dataset"]["target_transform"],
-            download=config["dataset"]["download"],
-            seed=self.seed,
-        )
+        return train_set, val_set, test_set
 
-        # initialize data loaders
-        train_loader = DataLoader(train_set, batch_size=config["train_val"]["train_batch_size"], shuffle=True)
-        val_loader = DataLoader(val_set, batch_size=config["train_val"]["val_batch_size"], shuffle=False)
-        test_loader = DataLoader(test_set, batch_size=config["test"]["test_batch_size"], shuffle=False)
-
-        return (train_set, val_set, test_set), (train_loader, val_loader, test_loader)
-
-    def _prepare_model(
-        self,
-        module_path: str,
-        module_cls: str,
-        weights: bool | str,
-    ) -> nn.Module:
+    def _prepare_model(self) -> nn.Module:
 
         # import model class
-        model_cls = getattr(self._import_package(module_path), module_cls)
+        model_cls = getattr(
+            self._import_package(f"{config["modules"]["model"]["root"]}.{config["modules"]["model"]["file"]}"),
+            config["modules"]["model"]["class"],
+        )
 
         # initialize the model
         model = model_cls(
+            arch=config["modules"]["model"]["params"]["arch"],
             in_features=config["dataset"]["image_shape"][0],
-            num_classes=self.num_classes,
-            weights=weights,
-            device=self.device,
-            verbose=self.verbose,
-            **config["modules"]["model"]["params"]["kwargs"],
+            num_classes=config["dataset"]["num_classes"],
+            weights=config["modules"]["model"]["params"]["weights"],
+            device=config["misc"]["device"],
+            verbose=config["misc"]["verbose"],
         )
 
         return model
 
-    def _train_and_validate(self) -> None:
-        train_acc_per_epoch = []
-        train_loss_per_epoch = []
-        val_acc_per_epoch = []
-        val_loss_per_epoch = []
+    def _train_and_validate(self, train_set, val_set, model: nn.Module) -> None:
+        # save stats per epoch
+        train_loss_per_epoch, train_acc_per_epoch = [], []
+        val_loss_per_epoch, val_acc_per_epoch = [], []
+
+        # initialize data loaders
+        train_loader = DataLoader(train_set, batch_size=config["train_val"]["train_batch_size"], shuffle=True)
+        val_loader = DataLoader(val_set, batch_size=config["train_val"]["val_batch_size"], shuffle=False)
 
         # initialize criterion, optimizer and lr_scheduler
         criterion = nn.CrossEntropyLoss()
-        optimizer: optim.Optimizer = config["train_val"]["optimizer"](self.model.parameters(), **config["train_val"]["optimizer_params"])
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, **config["train_val"]["scheduler_params"])
+        optimizer: optim.Optimizer = config["train_val"]["optimizer"](
+            model.parameters(),
+            **config["train_val"]["optimizer_params"],
+        )
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            **config["train_val"]["scheduler_params"],
+        )
 
-        # initialize train and validation accuracy metric (same concept as Clean Data Accuracy)
-        train_acc_metric = MulticlassAccuracy(self.num_classes).to(self.device)
-        val_acc_metric = MulticlassAccuracy(self.num_classes).to(self.device)
+        # initialize train and validation accuracy metric
+        train_acc_metric = MulticlassAccuracy(config["dataset"]["num_classes"]).to(config["misc"]["device"])
+        val_acc_metric = MulticlassAccuracy(config["dataset"]["num_classes"]).to(config["misc"]["device"])
 
         epochs = config["train_val"]["epochs"]
 
@@ -169,52 +147,72 @@ class NeutralRoutine(BaseRoutine):
 
         for epoch in range(1, epochs + 1):
             # train phase
-            self.model.train()
+            model.train()
             train_loss = 0
 
-            for x, y_true in self.train_loader:
-                x, y_true = x.to(self.device), y_true.to(self.device)
-                y_pred = self.model(x)
+            for x, y_true in train_loader:
+                # move data to <device>
+                x, y_true = x.to(config["misc"]["device"]), y_true.to(config["misc"]["device"])
+
+                # forward and backward pass
+                y_pred = model(x)
                 loss = criterion(y_pred, y_true)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
-                train_loss += loss.item() * len(x)
 
+                # calculate and store metrics per batch
+                train_loss += loss.item() * len(x)
                 train_acc_metric.update(y_pred, y_true)
 
-            train_loss /= len(self.train_loader.dataset)
+            # calculate metrics per epoch
+            train_loss /= len(train_loader.dataset)
             train_acc = train_acc_metric.compute().item()
+
+            # store metrics per epoch
             train_acc_per_epoch.append(train_acc)
             train_loss_per_epoch.append(train_loss)
+
+            # reset metrics for the next epoch
             train_acc_metric.reset()
 
             # validation phase
-            self.model.eval()
+            model.eval()
             val_loss = 0
 
             with torch.no_grad():
-                for x, y_true in self.val_loader:
-                    x, y_true = x.to(self.device), y_true.to(self.device)
-                    y_pred = self.model(x)
+                for x, y_true in val_loader:
+                    # move data to <device>
+                    x, y_true = x.to(config["misc"]["device"]), y_true.to(config["misc"]["device"])
+
+                    # forward pass
+                    y_pred = model(x)
                     loss = criterion(y_pred, y_true)
 
+                    # calculate and store metrics per batch
                     val_loss += loss.item() * len(x)
                     val_acc_metric.update(y_pred, y_true)
 
-            val_loss /= len(self.val_loader.dataset)
+            # calculate metrics per epoch
+            val_loss /= len(val_loader.dataset)
             val_acc = val_acc_metric.compute().item()
+
+            # store metrics per epoch
             val_acc_per_epoch.append(val_acc)
             val_loss_per_epoch.append(val_loss)
+
+            # reset metrics for the next epoch
             val_acc_metric.reset()
 
             # lr scheduler step
             scheduler.step(val_loss)
 
             # print results per epoch in the standard output
-            if self.verbose:
+            if config["misc"]["verbose"]:
                 print(
-                    f"[Train]: epoch {epoch:0{len(str(epochs))}}/{epochs} -> lr: {scheduler.get_last_lr()[0]:.5f} | train[loss: {train_loss:.5f} - acc: {train_acc*100:5.2f}%] | validation[loss: {val_loss:.5f} - acc: {val_acc*100:5.2f}%]"
+                    f"[Train]: epoch {epoch:0{len(str(epochs))}}/{epochs} -> lr: {scheduler.get_last_lr()[0]:.5f} | "
+                    f"train[loss: {train_loss:.5f} - acc: {train_acc*100:5.2f}%] | "
+                    f"validation[loss: {val_loss:.5f} - acc: {val_acc*100:5.2f}%]"
                 )
 
             # store metrics as a csv file for each epoch
@@ -233,8 +231,8 @@ class NeutralRoutine(BaseRoutine):
             logger.save_weights(
                 Path(config["log"]["weights"]["path"]),
                 f"epoch_{epoch}",
-                self.model,
-                only_state_dict=True,
+                model,
+                only_state_dict=config["log"]["weights"]["only_state_dict"],
             )
 
         # store and/or plot train and validation metrics per epoch
@@ -256,51 +254,64 @@ class NeutralRoutine(BaseRoutine):
                 **data,
             )
 
-        for n, d in [
-            ("train", self.train_set),
-            ("val", self.val_set),
-        ]:
+        # store and/or plot demo images with true and predicted labels
+        for data_role, data_value in [("train", train_set), ("val", val_set)]:
             logger.save_demo(
                 Path(config["log"]["demo"]["train_path"]),
-                n,
-                self.model,
-                d,
+                data_role,
+                model,
+                data_value,
                 config["log"]["demo"]["nrows"],
                 config["log"]["demo"]["ncols"],
                 show=config["log"]["demo"]["show"],
-                device=self.device,
+                device=config["misc"]["device"],
             )
 
-    def _test(self) -> None:
+    def _test(self, test_set, model: nn.Module) -> None:
+        # save true/pred labels for confusion matrix
         true_labels = []
         predictions = []
+
+        # initialize data loader
+        test_loader = DataLoader(test_set, batch_size=config["test"]["test_batch_size"], shuffle=False)
 
         # initialize criterion
         criterion = nn.CrossEntropyLoss()
 
-        # initialize test accuracy metric (same concept as Clean Data Accuracy)
-        test_acc_metric = MulticlassAccuracy(self.num_classes).to(self.device)
+        # initialize test acc (accuracy) metric
+        # move the metric to <device>
+        test_acc_metric = MulticlassAccuracy(config["dataset"]["num_classes"]).to(config["misc"]["device"])
 
-        self.model.eval()
+        # test phase
+        model.eval()
         test_loss = 0
 
         with torch.no_grad():
-            for x, y_true in self.test_loader:
-                x, y_true = x.to(self.device), y_true.to(self.device)
-                y_pred = self.model(x)
+            for x, y_true in test_loader:
+                # move data to <device>
+                x, y_true = x.to(config["misc"]["device"]), y_true.to(config["misc"]["device"])
+
+                # forward pass
+                y_pred = model(x)
                 loss = criterion(y_pred, y_true)
 
+                # calculate and store metrics per batch
                 test_loss += loss.item() * len(x)
                 test_acc_metric.update(y_pred, y_true)
 
+                # extend true/pred labels
                 true_labels.extend(y_true.cpu())
                 predictions.extend(y_pred.argmax(dim=1).cpu())
 
-        test_loss /= len(self.test_loader.dataset)
+        # calculate and store metrics
+        test_loss /= len(test_loader.dataset)
         test_acc = test_acc_metric.compute().item()
+
+        # reset metric
         test_acc_metric.reset()
 
-        if self.verbose:
+        # print results in the standard output
+        if config["misc"]["verbose"]:
             print(f"[Test]: test[loss: {test_loss:.5f} - acc: {test_acc*100:5.2f}%]")
 
         # store metrics as a csv file
@@ -313,24 +324,24 @@ class NeutralRoutine(BaseRoutine):
 
         # store confusion matrix as a csv file
         predictions, true_labels = map(torch.tensor, [predictions, true_labels])
-
-        confmat = MulticlassConfusionMatrix(self.num_classes)
+        confmat = MulticlassConfusionMatrix(config["dataset"]["num_classes"])
         cm = confmat(predictions, true_labels)
-
         logger.save_confusion_matrix(
             Path(config["log"]["confusion_matrix"]["path"]),
             config["log"]["confusion_matrix"]["filename"],
             cm,
-            list(range(self.num_classes)),
+            "True/Pred",
+            list(range(config["dataset"]["num_classes"])),
         )
 
+        # store and/or plot demo images with true and predicted labels
         logger.save_demo(
             Path(config["log"]["demo"]["test_path"]),
             "test",
-            self.model,
-            self.test_set,
+            model,
+            test_set,
             config["log"]["demo"]["nrows"],
             config["log"]["demo"]["ncols"],
             show=config["log"]["demo"]["show"],
-            device=self.device,
+            device=config["misc"]["device"],
         )
