4e0effa8dd71676fc5d850bd331caff69011149d
Amirhossein Heydari <amirhosseinheydari78@gmail.com>
2025-07-20 16:07:25 +0330

    Refactor models especially ResNet

--- src/backdoor_toolbox/models/cnn/mobilenet_wrapper.py
+++ src/backdoor_toolbox/models/cnn/mobilenet_wrapper.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 from torch import nn
+from torchinfo import summary
 from torchvision.models import MobileNet_V2_Weights, mobilenet_v2
 
 
@@ -26,32 +27,26 @@ class CustomMobileNetV2(nn.Module):
 
         model_fn, default_weights = self.MOBILENET_CONFIG[arch]
 
-        # Load model (pretrained or from checkpoint)
+        # load model (pretrained or from checkpoint)
         if weights is None:
             model = model_fn(weights=None)
         elif isinstance(weights, (str, Path)) and weights.endswith((".pt", ".pth")):
-            model = model_fn(weights=None)  # For manual checkpoint loading
+            model = model_fn(weights=None)  # for manual checkpoint loading
         else:
             model = model_fn(weights=default_weights)
             if verbose:
                 print(f"Loaded default pretrained weights for {arch}")
 
-        # Modify input conv if input channels ≠ 3
+        # modify input conv if input channels != 3
         if in_channels != 3:
             model.features[0][0] = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1, bias=False)
-
-        # Remove classifier
-        # self.features = nn.Sequential(*list(model.features.children())[:18])  # up to features[17]
-        # last_conv_out_channels = list(self.features.children())[-1].conv[-2].out_channels
-        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        # self.classifier = nn.Linear(last_conv_out_channels, num_classes)
 
         self.features = model.features
         last_conv_out_channels = model.classifier[1].in_features
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Linear(last_conv_out_channels, num_classes)
 
-        # Load custom weights if provided
+        # load custom weights if provided
         if isinstance(weights, (str, Path)) and weights.endswith((".pt", ".pth")):
             state_dict = torch.load(weights, map_location="cpu", weights_only=True)
             self.load_state_dict(state_dict)
@@ -71,18 +66,16 @@ class CustomMobileNetV2(nn.Module):
 
 
 if __name__ == "__main__":
+    input_size = (3, 32, 32)
+
     model = CustomMobileNetV2(
         arch="mobilenet_v2",
-        in_channels=1,
+        in_channels=input_size[0],
         num_classes=10,
         weights=None,
         device="cpu",
         verbose=True,
     )
 
-    # Print named parameters
-    for name, param in model.named_parameters():
-        print(f"{name}: {tuple(param.shape)}")
-
-    total_params = sum(p.numel() for p in model.parameters())
-    print(f"Total parameters: {total_params:,}")
+    model_summary = summary(model, input_size=(1, *input_size), device="cpu")
+    print(model_summary)
--- src/backdoor_toolbox/models/cnn/resnet_wrapper.py
+++ src/backdoor_toolbox/models/cnn/resnet_wrapper.py
@@ -1,111 +1,20 @@
-# from pathlib import Path
-
-# import torch
-# from torch import nn
-# from torchvision.models import (
-#     ResNet18_Weights,
-#     ResNet34_Weights,
-#     ResNet50_Weights,
-#     ResNet101_Weights,
-#     ResNet152_Weights,
-#     resnet18,
-#     resnet34,
-#     resnet50,
-#     resnet101,
-#     resnet152,
-# )
-# from torchvision.models.resnet import BasicBlock, ResNet
-
-
-# class CustomResNet(ResNet):
-#     RESNET_MODELS = {
-#         "resnet18": (resnet18, ResNet18_Weights.IMAGENET1K_V1, 256),
-#         "resnet34": (resnet34, ResNet34_Weights.IMAGENET1K_V1, 256),
-#         "resnet50": (resnet50, ResNet50_Weights.IMAGENET1K_V2, 1024),
-#         "resnet101": (resnet101, ResNet101_Weights.IMAGENET1K_V2, 1024),
-#         "resnet152": (resnet152, ResNet152_Weights.IMAGENET1K_V2, 1024),
-#     }
-
-#     def __init__(
-#         self,
-#         arch: str,
-#         in_channels: int,
-#         num_classes: int,
-#         weights: str | None,
-#         device: str = "cpu",
-#         verbose: bool = False,
-#     ):
-#         if arch not in self.RESNET_MODELS:
-#             raise ValueError(f"Unsupported arch '{arch}'. Choose from {list(self.RESNET_MODELS)}")
-
-#         model_fn, default_w, layer3_out = self.RESNET_MODELS[arch]
-
-#         # Initialize base ResNet with or without pretrained weights
-#         if weights is None:
-#             model = model_fn(weights=None)
-#         elif isinstance(weights, (str, Path)) and weights.endswith((".pt", ".pth")):
-#             model = model_fn(weights=None)  # for compatibility with our checkpoint
-#         else:
-#             model = model_fn(weights=default_w)
-#             if verbose:
-#                 print(f"Loaded default pretrained weights for {arch}")
-
-#         # Init base class
-#         super().__init__(BasicBlock, [len(b) for b in [model.layer1, model.layer2, model.layer3, model.layer4]])
-
-#         # Copy weights from base model
-#         self.load_state_dict(model.state_dict())
-
-#         # Adjust for small images
-#         self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
-#         self.maxpool = nn.Identity()
-#         self.layer4 = nn.Identity()
-#         self.fc = nn.Linear(layer3_out, num_classes)
-
-#         # Load checkpoint if path is given
-#         if isinstance(weights, (str, Path)) and weights.endswith((".pt", ".pth")):
-#             state_dict = torch.load(weights, map_location="cpu", weights_only=True)
-#             self.load_state_dict(state_dict)
-#             if verbose:
-#                 print(f"Loaded weights from {weights}")
-#         elif weights is None and verbose:
-#             print("No pretrained weights used")
-
-#         self.to(device)
-
-#     def forward(self, x):
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)  # identity
-
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
-#         x = self.layer4(x)  # identity
-
-#         # Apply global average pooling to get [B, C, 1, 1]
-#         x = nn.functional.adaptive_avg_pool2d(x, (1, 1))
-#         x = torch.flatten(x, 1)  # Now [B, C]
-#         x = self.fc(x)
-#         return x
-
-
 from pathlib import Path
 from typing import Literal
+
 import torch
 from torch import nn
+from torchinfo import summary
 from torchvision.models import (
-    resnet18,
-    resnet34,
-    resnet50,
-    resnet101,
-    resnet152,
     ResNet18_Weights,
     ResNet34_Weights,
     ResNet50_Weights,
     ResNet101_Weights,
     ResNet152_Weights,
+    resnet18,
+    resnet34,
+    resnet50,
+    resnet101,
+    resnet152,
 )
 from torchvision.models.resnet import ResNet
 
@@ -128,7 +37,7 @@ class CustomResNet(ResNet):
         device: torch.device | str = "cpu",
         verbose: bool = False,
     ) -> None:
-        # validate
+
         try:
             model_fn, default_w, layer3_out = self.RESNET_MODELS[arch]
         except KeyError:
@@ -196,20 +105,16 @@ class CustomResNet(ResNet):
 
 
 if __name__ == "__main__":
+    input_size = (3, 32, 32)
+
     model = CustomResNet(
-        arch="resnet34",
-        in_channels=1,
+        arch="resnet18",
+        in_channels=input_size[0],
         num_classes=10,
         weights=None,
         device="cpu",
         verbose=True,
     )
-    # model = resnet152(weights=None)
-    # model.layer4 = nn.Identity()
 
-    # Print named parameters
-    # for name, param in model.named_parameters():
-    #     print(f"{name}: {tuple(param.shape)}")
-
-    total_params = sum(p.numel() for p in model.parameters())
-    print(f"Total parameters: {total_params:,}")
+    model_summary = summary(model, input_size=(1, *input_size), device="cpu")
+    print(model_summary)
